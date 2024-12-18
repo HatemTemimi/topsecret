419530955625a7f6312cf89906ebb2dd3b6e0fc9
hatem.temimi <hatem.altemimi@gmail.com>
2024-12-18 11:22:05 +0100

    feat: details-modal-scaffold

---
+++ client/src/components/rentals/modal/detailsModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-dialog
+      v-model="dialog"
+      width="auto"
+      :persistent="false"
+    >
+      <v-card
+        max-width="400"
+        prepend-icon="mdi-update"
+        text="Your application will relaunch automatically after the update is complete."
+        title="Update in progress"
+      >
+      </v-card>
+  </v-dialog>
+</template>
+<script setup>
+import { inject } from 'vue';
+
+const dialog = inject("dialog")
+
+
+</script>
